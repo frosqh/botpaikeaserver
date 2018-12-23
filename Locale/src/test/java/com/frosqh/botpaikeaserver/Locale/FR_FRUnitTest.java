526feb910a9b35fabd0d8f065fe5a3d8b42c92e3
Frosqh <frosqh@gmail.com>
2018-12-23 17:17:40 +0100

    First test FR_FR

---
+++ Locale/src/test/java/com/frosqh/botpaikeaserver/Locale/FR_FRUnitTest.java
@@ -0,0 +1,12 @@
+package com.frosqh.botpaikeaserver.Locale;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FR_FRUnitTest {
+
+    @Test
+    public void firstTest(){
+        Assert.assertEquals("merde",new FR_FR().easterShit());
+    }
+}
